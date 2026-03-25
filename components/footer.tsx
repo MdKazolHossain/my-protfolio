6373773ce4eb490cebd938a949ca232c96bd2afc
MdKazolHossain <kajolkk5@gmail.com>
2026-03-24 23:35:40 -0700

    first commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -114,6 +114,7 @@ export function Footer() {
             </ul>
           </div>
         </div>
+        
 
         {/* Bottom Bar */}
         <div className="border-t border-background/10 mt-12 pt-8 flex flex-col sm:flex-row justify-between items-center gap-4">
