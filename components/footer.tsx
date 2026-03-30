469e8763c65c523ca1d794cd78019de3e5f95f6c
MdKazolHossain <kajolkk5@gmail.com>
2026-03-30 04:39:57 -0700

    ??

--- components/footer.tsx
+++ components/footer.tsx
@@ -43,6 +43,13 @@ export function Footer() {
               >
                 <Facebook className="w-5 h-5" />
               </a>
+              <a 
+                href="https://www.instagram.com/imonkhancmx/" 
+                className="w-10 h-10 bg-background/10 rounded-lg flex items-center justify-center hover:bg-primary transition-colors"
+                aria-label="Instagram"
+              >
+                <Instagram className="w-5 h-5" />
+              </a>
               {/* <a 
                 href="https://www.upwork.com/services/product/marketing-an-organic-growth-podcast-promotion-service-1810966061680305010" 
                 target="_blank"
@@ -59,15 +66,7 @@ export function Footer() {
               >
                 <Mail className="w-5 h-5" />
               </a>
-               <a 
-                href="https://www.instagram.com/imonkhancmx/" 
-                target="_blank"
-                rel="noopener noreferrer"
-                className="w-10 h-10 bg-background/10 rounded-lg flex items-center justify-center hover:bg-primary transition-colors"
-                aria-label="Instagram"
-              >
-                <Instagram className="w-5 h-5" />
-              </a>
+               
             </div>
           </div>
 
